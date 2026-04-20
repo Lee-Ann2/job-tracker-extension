b9e60f70896a6d0d2e4e14178707b09b57863880
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2026-04-20 17:21:09 +0200

    check.

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,3 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'openPopup') {
-        chrome.action.openPopup();
-    }
-});
-
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('Job Tracker Pro installed and ready');
+    console.log('Job Tracker Pro ready');
 });
